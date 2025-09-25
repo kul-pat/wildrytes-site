382846b7bfcab7e343378100b3d142d3100a466e
Kuldeep Pathoee <91526511+kul-pat@users.noreply.github.com>
2025-09-25 12:47:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_U1pRn3fUt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6akfv0d9g4jkoer1h126kopjg9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_v5lE6Vyyf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vadp7kr6dmm9g2dsgu33mhgko', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
